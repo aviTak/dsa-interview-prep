87be21b9723b183109f1abc895caa0be71836bdc
Pooja.Dudy <52880388+poojadudy@users.noreply.github.com>
2022-11-25 00:04:16 +0530

    Create 278FirstBadVersion.py

---
+++ LeetCode 75/Python LeetCode 75/278FirstBadVersion.py
@@ -0,0 +1,22 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        start = 0 
+        end = n 
+        while (end-start)>1:
+            mid = start + (end-start)//2
+            if isBadVersion(mid):
+                end = mid
+            else: 
+                start = mid
+        
+        return end
+                    
