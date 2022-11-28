9d56ee3bde42a52a13b9f4692acecfc74095f6c5
Pooja.Dudi <52880388+poojadudy@users.noreply.github.com>
2022-11-28 09:42:12 +0530

    Create 70Climbing Stairs.py

---
+++ LeetCode 75/Python LeetCode 75/70Climbing Stairs.py
@@ -0,0 +1,24 @@
+#70. Climbing Stairs
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1 or n == 2:
+            return n
+        dp  = [0 for i in range(n)]
+        dp[0] = 1
+        dp[1] = 2
+        for i in range(2,n):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n-1]
+#         if n == 1:
+#             return 1
+#         if n == 2:
+#             return 2
+        
+#         return self.climbStairs(n-1) + self.climbStairs(n-2)
+        
+        
