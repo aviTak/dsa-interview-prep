034192208dde2de953f2113de7ca58dc211d0632
Pooja.Dudi <52880388+poojadudy@users.noreply.github.com>
2022-12-05 00:07:42 +0530

    Update 202Happy Number.py

--- LeetCode 75/Python LeetCode 75/202Happy Number.py
+++ LeetCode 75/Python LeetCode 75/202Happy Number.py
@@ -36,4 +36,14 @@ A happy number is a number defined by the following process:
 Starting with any positive integer, replace the number by the sum of the squares of its digits.
 Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
 Those numbers for which this process ends in 1 are happy.
+
+        slow = self.square_number(n)
+        fast = self.square_number(self.square_number(n))
+
+        while slow!=fast and fast!=1:
+          # print(slow,fast)
+          slow = self.square_number(slow)
+          fast = self.square_number(self.square_number(fast))
+        
+        return fast==1
 """
