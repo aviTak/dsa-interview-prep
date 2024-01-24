380b6ebb4c1b998b7e6169fef5097a3a35b1a8c0
Pooja.Dudi <52880388+poojadudy@users.noreply.github.com>
2024-01-24 23:49:33 +0530

    1st commit at 24th jan 2024

--- JAN2024/fast_slow_pointer.py/cyclestart.py
+++ JAN2024/fast_slow_pointer.py/cyclestart.py
@@ -7,15 +7,19 @@ class Node:
 
 def has_cycle(head):
     fast = slow = head
+    print(fast.data,slow.data,'flflfl')
     cycle = 0
     while fast and fast.next:
+        # print(fast.next.data,'fastnextdata')
         fast = fast.next.next
         slow = slow.next
+        # print(slow.data,fast.data,'has_cycle')
         if slow == fast:
+            # print(f"{slow.data},{fast.data}")
             cycle =  calcyclelength(slow)
             break
     return findstart(head, cycle)
-    
+    # return cycle
 
 def calcyclelength(slow):
     current = slow
@@ -24,30 +28,41 @@ def calcyclelength(slow):
     while True:
         current = current.next
         cyclelength += 1
+        # print(current.data,slow.data,'current_slow')
         if current == slow:
             break
-    
     return cyclelength
 
 def findstart(head,cyclelength):
     pointer1 = head
     pointer2 = head
-    while cyclelength > 0:
+    if cyclelength == 0:
+        return -1
+    while cyclelength>0:
         pointer2 = pointer2.next
-        cyclelength -= 1
-    
-    while pointer2 != pointer1:
+        cyclelength -=1
+    print(pointer2.data,'datllt')
+    while pointer1 != pointer2:
+        pointer2 = pointer2.next
         pointer1 = pointer1.next
-        pointer2 = pointer2.next
+        print(pointer2.data,pointer1.data)
     
-    return pointer1
+    return pointer1.data
 
 
-head = Node(1)
+def display(head):
+    temp = head
+    while temp:
+        print(temp.data)
+        temp = temp.next
+    
+
+head = Node(3)
 head.next = Node(2)
-head.next.next = Node(3)
-head.next.next.next = Node(4)
+head.next.next = Node(0)
+head.next.next.next = Node(-4)
 head.next.next.next.next = Node(5)
-print(has_cycle(head).data)
-head.next.next.next.next = head.next.next
-print(has_cycle(head).data)
+# print(has_cycle(head))
+# print(display(head))
+head.next.next.next.next.next = head.next
+print(has_cycle(head))
