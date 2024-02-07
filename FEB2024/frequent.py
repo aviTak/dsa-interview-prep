5357cd9fa32a48131ab15e2a70649453dc2c6fc9
Pooja.Dudi <52880388+poojadudy@users.noreply.github.com>
2024-02-07 23:56:14 +0530

    1st question on 7th feb 2024

---
+++ FEB2024/frequent.py
@@ -0,0 +1,31 @@
+#find most frequent element in array
+arr = [2, 12, 1, 2, 8, 1, 1, 1, 8, 2]
+def FrequentArray(arr):
+    dict1 = {}
+    element = 0
+    maxi = 0
+    for ele in arr:
+        if ele in dict1:
+            dict1[ele] += 1
+            if maxi < dict1[ele]:
+                maxi = dict1[ele]
+                element = ele
+            else:
+                element = min(element,ele)
+        else:
+            dict1[ele] = 1
+            element = ele
+    
+    print(dict1)
+    # for key,value in dict1.items():
+    #     count_freq = value
+    #     if count_freq > maxi:
+    #         element = key
+    #         maxi = count_freq
+    #     elif count_freq == maxi:
+    #         element = min(element,key)
+    
+    return element
+
+        
+FrequentArray(arr)
