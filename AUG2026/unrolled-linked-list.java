d1e9d44bf5bb6b0487f4d4bc648a773c135b3d17
Avi Takiyar <avi.takiyar@gmail.com>
2026-08-22 20:05:15 +0530

    Create unrolled-linked-list.java

---
+++ AUG2026/unrolled-linked-list.java
@@ -0,0 +1,83 @@
+import java.util.*;
+
+class UnrolledLinkList {
+    class Node {
+        Node next;
+        ArrayList<Integer> values;
+
+        Node() {
+            next = null;
+            values = new ArrayList<>();
+        }
+    }
+
+    private final int capacity;
+    private Node head;
+
+    public UnrolledLinkList(int capacity) {
+        head = null;
+        this.capacity = capacity;
+    }
+
+    public void add(int value) {
+        if (capacity <= 0) {
+            return;
+        }
+
+        if (head == null) {
+            head = new Node();
+            head.values.add(value);
+
+            return;
+        }
+
+        Node curr = head;
+
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+
+        if (curr.values.size() < capacity) {
+            curr.values.add(value);
+        } else {
+            Node node = new Node();
+            node.values.add(value);
+
+            curr.next = node;
+        }
+    }
+
+    public boolean delete(int value) {
+        Node curr = head;
+
+        while(curr != null) {
+            if (curr.values.contains(value)) {
+                curr.values.remove(Integer.valueOf(value));
+
+                if (curr.values.isEmpty()) {
+                    removeNode(curr);
+                }
+
+                return true;
+            }
+            curr = curr.next;
+        }
+
+        return false;
+    }
+
+    private void removeNode(Node node) {
+        if (node == head) {
+            head = head.next;
+            return;
+        }
+
+        Node curr = head;
+
+        while (curr.next != node) {
+            curr = curr.next;
+        }
+
+        curr.next = node.next;
+    }
+}
