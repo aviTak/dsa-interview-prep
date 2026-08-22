581ddb9bb109290ee2a44c189bfca81db8e970af
Avi Takiyar <avi.takiyar@gmail.com>
2026-08-22 21:56:33 +0530

    Update reverse-ll.java

--- AUG2026/reverse-ll.java
+++ AUG2026/reverse-ll.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class LinkedList {
-    class Node {
+    static class Node {
         int value;
         Node next;
 
@@ -11,7 +11,7 @@ class LinkedList {
         }
     }
 
-    private Node head;
+    Node head;
 
     public LinkedList() {
         head = null;
@@ -50,16 +50,61 @@ class LinkedList {
 
 class Solution {
     public static void main(String args[]) {
-        LinkedList list = new LinkedList();
+        LinkedList list1 = new LinkedList(),
+            list2 = new LinkedList();
 
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
+        
+        // listA = [-3,-1,9,10], listB = [-10,3,4,6,9]
 
-        list.display();
-        list.reverse();
-        list.display();
+        list1.add(10);
+        list1.add(9);
+        list1.add(-1);
+        list1.add(-3);
+
+        list1.display();
+
+        list2.add(9);
+        list2.add(6);
+        list2.add(-4);
+        list2.add(3);
+        list2.add(-10);
+
+        list2.display();
+
+        LinkedList.Node merged = linkedListCombineTwoSorted(list1.head, list2.head);
+        display(merged);
     }
+
+    public static void display(LinkedList.Node head) {
+        LinkedList.Node curr = head;
+
+        while (curr != null) {
+            System.out.println(curr.value);
+            curr = curr.next;
+        }
+    }
+
+    public static LinkedList.Node linkedListCombineTwoSorted(LinkedList.Node l1, LinkedList.Node l2) {
+        LinkedList.Node dummy = new LinkedList.Node(0);
+        LinkedList.Node tail = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.value <= l2.value) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
+            }
+
+            tail = tail.next;
+        }
+
+        tail.next = l1 == null? l2 : l1;
+
+        return dummy.next;
+    }
+
+
 }
 
